0a23682fa9b1884c634b11f9bec6afacbd77912a
Wilfred Muchire <logincoin40@gmail.com>
2024-08-15 09:33:46 +0300

    setting up the flashcard-saas application

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAmAR9-MTDooiOakkFaS8-CKl1xP-yIBvw",
+  authDomain: "login-authentication-app-42044.firebaseapp.com",
+  databaseURL:
+    "https://login-authentication-app-42044-default-rtdb.firebaseio.com",
+  projectId: "login-authentication-app-42044",
+  storageBucket: "login-authentication-app-42044.appspot.com",
+  messagingSenderId: "904104578744",
+  appId: "1:904104578744:web:04c00572db267f0b55ee4d",
+  measurementId: "G-8XQM9E24GP",
+};
+
+const app = initializeApp(firebaseConfig);
